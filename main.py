7f868f97cfd81c3c649d164aba1f41f04e0f390b
noraiz12345 <cnoraizgujjar@gmail.com>
2025-06-16 18:16:39 +0500

    task in this file

--- main.py
+++ main.py
@@ -32,14 +32,61 @@ def stack_operations():
             print("Invalid choice.")
 
 def queue_operations():
+    queue = []
     # TODO: Implement queue using list or collections.deque
     print("\n-- Queue Operations --")
-    print("TODO: Implement this feature")
+    while True:
+        print("1. enqueue\n2. dequeue\n3. Display\n4. Back to Main Menu")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            item = input("Enter item to enqueue: ")
+            queue.append(item)
+        elif choice == "2":
+            if stack:
+                print("dequeued item:", queue.pop(0))
+            else:
+                print("queue is empty.")
+        elif choice == "3":
+            print("queue content:", queue)
+        elif choice == "4":
+            break
+        else:
+            print("Invalid choice.")
 
 def dictionary_operations():
-    # TODO: Implement basic dictionary operations like add, delete, search, display
+    dict = {}
     print("\n-- Dictionary Operations --")
-    print("TODO: Implement this feature")
+    while True:
+        print("1. insert\n2. delete\n3. Display\n4. search\n5. Back to Main Menu")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            item = input("Enter item : ")
+            key = input("Enter key : ")
+            dict[key] = item
+        elif choice == "2":
+            key = input("Enter key to delete: ")
+            if key in dict:
+                del dict[key]
+                print(f"Deleted key: {key}")
+            else:
+                print("Key not found.")
+        elif choice == "3":
+            if dict:
+                print("Dictionary contents:")
+                for key, value in dict.items():
+                    print(f"{key}: {value}")
+            else:
+                print("Dictionary is empty.")
+        elif choice == "4":
+            key = input("Enter key to search: ")
+            if key in dict:
+                print(f"Found: {key} => {dict[key]}")
+            else:
+                print("Key not found.")
+        elif choice == "5":
+            break
+        else:
+            print("Invalid choice.")
 
 def main():
     while True:
